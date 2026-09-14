0e54bd45e875eccd3c1b6005c541f35fba8c334c
mmonirweb <mmonirujjaman27@gmail.com>
2026-09-14 21:36:33 +0600

    Fix: Updated API base URL to sub-domain

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import Cookies from 'js-cookie';
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://nrgsolarbd.com/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://api.nrgsolarbd.com/api/v1',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
